e16b8f815bcb4eb24ed7576627895f631bf82002
Marc Dominik Migge <mdm@sms-digital.com>
2018-06-20 11:55:04 +0200

    fix(handshake): Turn check for upgrade header into a case-insensitive regex

--- lib/em-websocket/handler_factory.rb
+++ lib/em-websocket/handler_factory.rb
@@ -69,7 +69,7 @@ module EventMachine
         end
 
         # Validate that Connection and Upgrade headers
-        unless request['connection'] && request['connection'].downcase == 'upgrade' && request['upgrade'] && request['upgrade'].downcase == 'websocket'
+        unless request['connection'] && request['connection'] =~ /upgrade/i && request['upgrade'] && request['upgrade'].downcase == 'websocket'
           raise HandshakeError, "Connection and Upgrade headers required"
         end
 
